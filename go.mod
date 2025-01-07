79bcd50407f527aeee847cab4ed8bb90e51b6d07
Dmitry Ignatovskij <dignatovskij102@gmail.com>
2025-01-07 16:47:56 +0300

    Переход на другую библиотеку

--- go.mod
+++
@@ -1,5 +0,0 @@
-module TG_bot
-
-go 1.23.1
-
-require gopkg.in/telebot.v4 v4.0.0-beta.4 // indirect
--- main.go
+++ main.go
@@ -1,33 +1,5 @@
 package main
 
-import (
-	"log"
-	"os"
-	"time"
-
-	tg "gopkg.in/telebot.v4"
-)
-
 func main() {
-	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
-	err := os.Setenv("TOKEN", "7743496303:AAFdoSLRcTEyXSknBQbPtBwCbgbBlfrpN4U")
-	if err != nil {
-		log.Fatalln("Ошибка создания переменной окружения", err)
-	}
-	pref := tg.Settings{
-		Token:  os.Getenv("TOKEN"),
-		Poller: &tg.LongPoller{Timeout: 10 * time.Second},
-	}
 
-	b, err := tg.NewBot(pref)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	b.Handle("/start", func(c tg.Context) error {
-		return c.Send("Привет!")
-	})
-
-	b.Start()
 }
